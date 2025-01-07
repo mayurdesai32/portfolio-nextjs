df8083e594e8e317b079886c5eca5d57d30f1083
piyanshugehani <piyanshugehani@gmail.com>
2025-01-07 10:00:19 +0530

    fixed

--- src/component/Model.js
+++ src/component/Model.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export function Model(props) {
+export default function Model(props) {
   const { nodes, materials } = useGLTF("./model.glb");
   return (
     <group {...props} dispose={null}>
