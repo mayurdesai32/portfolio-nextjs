985ef93770e6dfc8aea4a414cc086c0ab7daad8d
Mayur desai <msdesai32@gmail.com>
2025-01-08 15:47:06 +0530

    added

--- src/component/HeroImage3d.js
+++ src/component/HeroImage3d.js
@@ -82,8 +82,6 @@ setScale(1.8)
         height: "100%",
       }}
     >
-
-    
       <Canvas
         flat
         antialias={"true"}
@@ -93,15 +91,15 @@ setScale(1.8)
           width: "100%",
           height: "100%",
           position: "absolute",
-   
-          top:scale<1.9?(scale<1.4?"0%" :"-7%"):'7%',
+
+          top: scale < 1.9 ? (scale < 1.4 ? "-7%" : "17%") : "4%",
           zIndex: 2,
         }}
       >
         <Suspense fallback={<Loader scale={scale} />}>
           <Environment preset="apartment" />
           <ambientLight intensity={2} />
-          <OrbitControls enableZoom={false} />
+          <OrbitControls enableZoom={false}  />
           <Model scale={[scale, scale, scale]} />
         </Suspense>
       </Canvas>
